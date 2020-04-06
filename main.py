b49527616728c3df8305509d2323fccbd74ab025
Kirill Timakov <Tim1997.10@yandex.ru>
2020-04-06 17:53:06 +0300

    Main

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+def read_the_file(dir_path):
+    with open(dir_path, 'r') as file:
+        for line in file:
+            if '+' in line:
+                index = line.find('+')
+                convert(line[index:])
+
+
+def main():
+    dir_path = str(input("Введите полный путь до файла и название файла:\n"))
+    try:
+        read_the_file(dir_path)
+    except:
+        print("Возможно путь был указан неправильно или файл не существует.\nПопробуйте еще раз")
+        main()
+
+if __name__ == "__main__":
+    main()
